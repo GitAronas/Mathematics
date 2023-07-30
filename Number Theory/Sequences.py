a1550c6f6f25001234f8b07facc5fad3ae883f52
Amar Doshi <97429962+GitAronas@users.noreply.github.com>
2023-07-30 13:59:51 +0530

    Update Sequences.py

--- Number Theory/Sequences.py
+++ Number Theory/Sequences.py
@@ -5,7 +5,7 @@ Created on Mon Jan 16 18:48:29 2023
 @author: Amar Doshi
 """
 
-def fibonacci(n: int) -> list:
+def fibonacci(n: int) -> list[int]:
     '''
     Gives the first n terms of the Fibonacci sequence
     '''
@@ -22,7 +22,7 @@ def fibonacci(n: int) -> list:
     return f
 
 
-def collatz(startingNumber):
+def collatz(startingNumber: int) -> list[int]:
     if startingNumber < 1: return []
 
     s = [startingNumber]
@@ -36,3 +36,14 @@ def collatz(startingNumber):
             s.append(startingNumber)
 
     return s
+
+
+def triangularNumbers(n: int) -> list[int]:
+    m = 0
+    t = []
+    
+    for i in range(1, n + 1):
+        m += i
+        t.append(m)
+        
+    return t
